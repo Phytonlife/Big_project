93731e51f808f3442640163edc5e111c1413ea10
lvlit <player14017@gmail.com>
2024-12-29 08:00:44 +0500

    Сделали админку и в админке сделали автоматическое заполнение слагов

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product,Category
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',"parent" ,'slug')
+    ordering = ('name',)
+
+    def get_prepopulated_fields(self, request, obj=None):#Создание уникального слага автоматически
+        return {"slug": ("name",)}
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title',"brand",'slug','price','aviable','category','created_at','updated_at')
+    list_filter = ('aviable',"created_at",'updated_at')
+    ordering = ('title',)
+
+    def get_prepopulated_fields(self, request, obj=None):#Создание уникального слага автоматически
+        return {"slug": ("title",)}
